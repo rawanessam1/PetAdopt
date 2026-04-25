b3e9992b383f2e0defae54bd05e7f43c91d944bb
Rawda Abdullah <rawdaabdullah45@gmail.com>
2026-04-25 18:43:24 +0300

    added adoptionReq

---
+++ backend/backend/Models/adoptionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class adoptionRequest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int PetId { get; set; }
+        public string AdopterId { get; set; }
+        public string Status { get; set; } // Pending, Accepted, Rejected
+        public DateTime CreatedAt { get; set; }
+    }
+}
